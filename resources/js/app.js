4d1364d422ae2de815ce8a4c8fec7c00ad927dd7
Leonardo <dev@jointech.io>
2024-12-11 10:32:53 -0300

    feat: add alpine magic deleteModal

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,4 +4,11 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
+document.addEventListener('alpine:init', () => {
+    Alpine.magic('deleteModal', () => route => {
+        confirm('Confirm exclusion?') && axios.delete(route).then(() => window.location.reload())
+    })
+})
+
+
 Alpine.start();
